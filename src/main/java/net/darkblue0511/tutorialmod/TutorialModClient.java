ac7e0867a9a5dd434bba48730f8c8538fc8a6b1d
dohan0511 <dohan0511@naver.com>
2025-05-06 22:22:13 +0900

    Initial Commit

---
+++ src/main/java/net/darkblue0511/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.darkblue0511.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
